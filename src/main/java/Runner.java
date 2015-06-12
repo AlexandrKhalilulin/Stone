c0a5605f6c17f911bdab82e8e9dc053ac438977d
AlexandrKhalilulin <akhalilulin@mail.ru>
2015-06-12 13:56:11 +0600

    Change project Stone

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -5,10 +5,8 @@ import logic.NecklaceLogic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
@@ -18,5 +16,5 @@ public class Runner {
         logger.info("Necklace weight is: {}", String.valueOf(necklace.necklaceWeight()));
         Collections.sort(necklace.getStones(), Comparator.comparing(Stone::getUUID));
         logger.info("Stone in range by transparency is: {}", String.valueOf(NecklaceLogic.searchStonesByTransparency(necklace)));
-        }
+    }
 }
